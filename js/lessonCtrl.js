87df7c4eb77e88d53b1d5f1688ed28faf680d6b8
Rachel Ruff <rachelruff@Rachels-MacBook-Pro.local>
2018-03-22 16:42:04 -0500

    Step 8

---
+++ js/lessonCtrl.js
@@ -0,0 +1,19 @@
+angular.module("directivePractice").controller("lessonCtrl", function($scope) {
+  $scope.name = "Rachel";
+  $scope.test = "Two-way data binding!";
+  $scope.lessons = [
+    "Services",
+    "Routing",
+    "Directives",
+    "Review",
+    "Firebase",
+    "No server project",
+    "Node",
+    "Express",
+    "Mongo"
+  ];
+  $scope.announceDay = function(lesson, day) {
+    alert( lesson + " is active on " + day + ".");
+  };
+
+});
